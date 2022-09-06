87fb94be077e8d2987e23e6ea82a19d7794bddb7
Maksim <C:\Users\User\AppData\Roaming\The Bat!>
2022-09-06 16:28:29 +0400

    add Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное 
+// число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите три числа:");
+int number = int.Parse(Console.ReadLine());
+int numberPosition2 =(number/10)%10;
+
+Console.WriteLine($"Второе число: {numberPosition2}");
+
