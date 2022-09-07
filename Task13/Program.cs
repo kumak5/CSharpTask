a147a659300d680f8d0030e3804dfd1bd3665fa6
Maksim <C:\Users\User\AppData\Roaming\The Bat!>
2022-09-07 08:56:11 +0400

    add Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int trueNumber = int.Parse(number);
+int lengthNamber = number.Length;
+// number = number/10;
+if (lengthNamber <3)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    for (int i = lengthNamber; i > 3; i--)
+    {
+        trueNumber = trueNumber / 10;
+    }
+Console.WriteLine($"Третье число: {trueNumber % 10}");
+}
