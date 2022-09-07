f939afe8e13d7a0dd03f140b5d960b955880c1e7
Maksim <C:\Users\User\AppData\Roaming\The Bat!>
2022-09-07 09:40:25 +0400

    add Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+Console.WriteLine("Введите число дня недели: ");
+int day = int.Parse(Console.ReadLine());
+if (day != 1 && day != 2 && day != 3 && day != 4 && day != 5 && day != 6 && day != 7)
+{
+    Console.WriteLine("нужно ввести число от 1 до 7");
+}
+else if (day == 6 || day == 7)
+{
+    Console.WriteLine("этот день выходной");
+}
+else
+{
+    Console.WriteLine("этот день будний");
+}
+
