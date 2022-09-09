236b7b901fc58c2021a5db06e266a67a23df5c4e
Maksim <C:\Users\User\AppData\Roaming\The Bat!>
2022-09-09 11:08:29 +0400

    add Task21

---
+++ Task21/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("Введите координаты двух точек в 3-х осевой коодинатной системе: ");
+Console.WriteLine("первая точка: ");
+
+
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("вторая точка: ");
+
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+
+double result = Math.Round((Math.Sqrt(Math.Pow((x2 - x1), 2) +
+                            Math.Pow((y2 - y1), 2) +
+                            Math.Pow((z2 - z1), 2))), 2);
+
+
+Console.WriteLine($"Растояние от точки до точки в пространстве: {result}");
