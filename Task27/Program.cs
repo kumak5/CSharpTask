30c535875a4a18b970e85302da288a1376b0414a
Maksim Ku <my0yashik@gmail.com>
2022-09-13 01:06:00 +0400

    add Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число: ");
+
+int GetSumNumber(int numberA)
+{
+    int sum = 0;
+    int nBB = Math.Abs(numberA);
+    int digit;
+    while (nBB > 0)
+    {
+        digit = nBB % 10;
+        sum = sum + digit;
+        nBB = nBB / 10;
+    }
+    return sum;
+}
+
+try
+{
+    int nA = int.Parse(Console.ReadLine());
+
+    int sum = GetSumNumber(nA);
+    Console.WriteLine($"результат: {sum}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Входное значение имеет не верный формат.");
+    Console.WriteLine(ex.Message);
+}
