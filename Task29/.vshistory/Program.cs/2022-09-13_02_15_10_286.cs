e55d15f5705f7012f22006596af87dfabf45a4f6
Maksim Ku <my0yashik@gmail.com>
2022-09-14 02:46:31 +0400

    add Task29

---
+++ Task29/.vshistory/Program.cs/2022-09-13_02_15_10_286.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и 
+// выводит их на экран. Элементы массива вводятся пользователем.
+
+Console.WriteLine("Введите восемь чисел: ");
+
+// int[] GetArray(int[] arrayD)
+// {
+//     // int[] arrayD = new int[7];
+//     for (int i = 0; i < arrayD.Length; i++)
+//     {
+//         arrayD[i] = arrayD;
+//     }
+//     return arrayD;
+// }
+
+void PrintArrayToConsole(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+
+}
+
+try
+{
+
+    int[] arrayDigits = new int[7];
+
+    for (int i = 0; i < 8; i++)
+    {
+        arrayDigits[i] = int.Parse(Console.ReadLine());
+    }
+
+    PrintArrayToConsole(arrayDigits);
+    // PrintArrayToConsole(array);
+}
+catch (Exception)
+{
+
+}
+
+
---
+++ Task29/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и 
+// выводит их на экран. Элементы массива вводятся пользователем.
+
+Console.WriteLine("Введите восемь чисел: ");
+
+void PrintArrayToConsole(int[] array)
+{
+    Console.Write("Массив из восьми чисел: ");
+    for (int i = 0; i < 8; i++)
+    {
+        System.Console.Write(array[i]);
+        System.Console.Write(" ");
+    }
+}
+
+try
+{
+    int[] arrayDigits = new int[8];
+
+    for (int i = 0; i < 8; i++)
+    {
+        arrayDigits[i] = int.Parse(Console.ReadLine());
+    }
+
+    PrintArrayToConsole(arrayDigits);
+}
+catch (System.Exception)
+{
+
+    throw;
+}
