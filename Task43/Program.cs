e1144e9201356b08c8bdfcf262f2338ecf69fecc
Maksim Ku <my0yashik@gmail.com>
2022-09-18 20:45:28 +0400

    add Task43

---
+++ Task43/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("Введите 4 числа: ");
+
+
+double b1;
+double b2;
+double k1;
+double k2;
+double x, y;
+
+try
+{
+     b1 = int.Parse(Console.ReadLine());
+     k1 = int.Parse(Console.ReadLine());
+     b2 = int.Parse(Console.ReadLine());
+     k2 = int.Parse(Console.ReadLine());
+}
+catch (System.Exception)
+{
+    Console.WriteLine("Введите число!");
+    throw;
+}
+
+x = (b1-b2)/(k2-k1);
+y = k2*x+b2;
+
+Console.WriteLine($"Результат : {x} {y}");
