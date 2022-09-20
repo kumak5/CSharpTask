221f8b710d40ab62cf39e51bcca43bf4124385e3
Maksim <C:\Users\User\AppData\Roaming\The Bat!>
2022-09-20 11:17:49 +0400

    edit Task38 edit finding the maximum and minimum number

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -8,7 +8,8 @@ Console.WriteLine("Введите размер массива: ");
 
 
 int sizeArray = 0;
-
+int max = 0;
+int min = 0;
 
 try
 {
@@ -37,27 +38,25 @@ int[] getRandomArray()
 
 int differenceNumberInArray(int[] array)
 {
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min) min = array[i];
 
-    int first = array[0];
-    int second = array[sizeArray - 1];
-    int arrayDifference;
-    if (array[0] > array[sizeArray - 1])
-    {
-        arrayDifference = array[0] - array[sizeArray - 1];
+        if (array[i] > max) max = array[i];
     }
-    else
-    {
-        arrayDifference = array[sizeArray - 1] - array[0];
-    }
-    Console.WriteLine($"Первое число: {first}");
-    Console.WriteLine($"Второе число: {second}");
-    return arrayDifference;
+
+
+    // int first = array[0];
+    // int second = array[sizeArray - 1];
+    int numberDifference = max - min;
+
+    return numberDifference;
 }
 
-void outputResultConsole(int arrayDifference)
+void outputResultConsole(int namberDifference)
 {
 
-    Console.Write($"Разница между первым и последним чисвлом в массиве из {sizeArray} чисел:  {arrayDifference} ");
+    Console.Write($"Разница между максимальным и минимальным чисвлом в массиве из {sizeArray} чисел:  {namberDifference} ");
 
     Console.WriteLine(" ");
     Console.WriteLine(" ");
