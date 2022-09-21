40fec08cd2f1c8532edc619b878157778b22b5ed
Maksim <C:\Users\User\AppData\Roaming\The Bat!>
2022-09-21 15:54:14 +0400

    add Task47

---
+++ Task47/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+int n = 0;
+int m = 0;
+
+Console.Write("Введите количество строк (N): ");
+
+bool parseNIsOk = int.TryParse(Console.ReadLine(), out int numberN);
+
+if (!parseNIsOk)
+{
+    Console.WriteLine("Введено значение некорректного формата (N)");
+}
+else
+{
+    n = numberN;
+}
+
+Console.Write("Введите количество столбцов (M): ");
+
+bool parseMIsOk = int.TryParse(Console.ReadLine(), out int numberM);
+
+if (!parseMIsOk)
+{
+    Console.WriteLine("Введено значение некорректного формата (M)");
+}
+else
+{
+    m = numberM;
+}
+
+Console.WriteLine("");
+// Console.WriteLine($"Введите количество строк (M): {m}");
+
+int[,] array = new int[n, m];
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        array[i, j] = new Random().Next(1, 10);
+    }
+}
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        Console.Write(array[i, j]);
+    }
+    Console.WriteLine(" ");
+}
