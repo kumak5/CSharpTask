0c84cd9d1bdb3588270d74fbc7017f46ef2ff317
Maksim <C:\Users\User\AppData\Roaming\The Bat!>
2022-09-29 16:29:18 +0400

    add Task60

---
+++ Task60/Program.cs
@@ -0,0 +1,28 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+// int[,,] array = { { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } } };
+
+int[,,] array = new int[2, 2, 2];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int k = 0; k < array.GetLength(2); k++)
+        {
+            array[i, j, k] = new Random().Next(10, 100);
+        }
+    }
+}
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int k = 0; k < array.GetLength(2); k++)
+        {
+            Console.Write($"{array[i, j, k]}({i},{j},{k}) ");
+        }
+        Console.WriteLine(" ");
+    }
+}
